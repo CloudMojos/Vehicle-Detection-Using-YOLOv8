c777b58d4c5163f95a03bb16b0b95a021eaaafa9
starfart69 <joshuel.simbulan@gmail.com>
2024-04-09 16:46:03 +0800

    some changes

--- server/detection.py
+++ server/detection.py
@@ -2,51 +2,54 @@ from ultralytics import YOLO
 import cv2
 import math
 
+
 def video_detection(path_x):
     video_capture = path_x
-    #Create a Webcam Object
-    cap=cv2.VideoCapture(video_capture)
-    if (path_x == 0):
-        cap.open("http://192.168.89.224:8080/video")
-    frame_width=int(cap.get(3))
-    frame_height=int(cap.get(4))
-    #out=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P','G'), 10, (frame_width, frame_height))
+    # Create a Webcam Object
+    cap = cv2.VideoCapture(video_capture)
+    # if (path_x == 0):
+    #     cap.open("http://192.168.89.224:8080/video")
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    # out=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P','G'), 10, (frame_width, frame_height))
 
-    model=YOLO("../weights/yolov8n.pt")
-    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-                  "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-                  "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-                  "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-                  "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-                  "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-                  "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-                  "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-                  "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-                  "teddy bear", "hair drier", "toothbrush"
-                  ]
+    model = YOLO("../weights/yolov8n.pt")
+    class_names = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+                   "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+                   "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+                   "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                   "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                   "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+                   "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+                   "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                   "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                   "teddy bear", "hair drier", "toothbrush"
+                   ]
     while True:
         success, img = cap.read()
-        results=model(img,stream=True)
+        results = model(img, stream=True)
         for r in results:
-            boxes=r.boxes
+            boxes = r.boxes
             for box in boxes:
                 cls = int(box.cls[0])
                 if 1 < cls < 7:
                     x1, y1, x2, y2 = box.xyxy[0]
                     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                    class_name=classNames[cls]
+                    class_name = class_names[cls]
                     conf = math.ceil((box.conf[0] * 100)) / 100
-                    label=f'{class_name}{conf}'
+                    label = f'{class_name}{conf}'
                     t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
                     print(t_size)
                     c2 = x1 + t_size[0], y1 - t_size[1] - 3
-                    cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
+                    cv2.rectangle(img, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)  # filled
                     cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-                    cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
+                    cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
         yield img
-        #out.write(img)
-        #cv2.imshow("image", img)
-        #if cv2.waitKey(1) & 0xFF==ord('1'):
-            #break
-    #out.release()
+        # out.write(img)
+        # cv2.imshow("image", img)
+        # if cv2.waitKey(1) & 0xFF==ord('1'):
+        # break
+    # out.release()
+
+
 cv2.destroyAllWindows()
